fb6137671c1b303f836f6786f141230e288e8521
Sean Hagstrom <sean@seanhagstrom.com>
2022-06-12 16:53:48 +0100

    add elm configuration to webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,18 @@ module.exports = (env) => {
 	// Learn how to customize:
 	// https://docs.nativescript.org/webpack
 
+	webpack.chainWebpack(config => {
+		config.module
+			.rule("elm")
+			.test(/\.elm$/)
+			.use('elm-webpack-loader')
+			.loader('elm-webpack-loader')
+			.options({
+				debug: false,
+				pathToElm: 'node_modules/.bin/elm'
+			})
+	})
+
 	return webpack.resolveConfig();
 };
-
 
